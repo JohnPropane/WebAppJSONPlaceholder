ebf155f5256a142d547bfe2aabbf96b6ee9d7131
Hubert Muszalski <147393823+JohnPropane@users.noreply.github.com>
2024-04-13 23:51:48 +0200

    Strona z postami
    Ukończona strona z postami + filtrowanie postów

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import web_functions
 
 app = Flask(__name__)
 
@@ -8,9 +9,17 @@ def home():
     return render_template("home.html")
 
 
-@app.route('/posts')
+@app.route('/posts', methods=['GET', 'POST'])
 def posts():
-    return render_template("posts.html")
+    if request.method == 'POST':
+        num_of_posts = int(request.form['max-posts'])
+        min_len = int(request.form['min-len'])
+        max_len = int(request.form['max-len'])
+        data = web_functions.generate2(num_of_posts, min_len, max_len)
+    else:
+        num_of_posts = 100
+        data = web_functions.generate2(num_of_posts)
+    return render_template('posts.html', data=data)
 
 
 @app.route('/albums')
