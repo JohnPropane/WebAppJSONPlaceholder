9cfc6cf6029fdd8f01763fce09ea0fefec0f2695
Hubert Muszalski <147393823+JohnPropane@users.noreply.github.com>
2024-04-13 23:36:49 +0200

    Strona główna
    Ukończona strona główna i szkielet kolejnych podstron

--- main.py
+++ main.py
@@ -2,9 +2,21 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def home():
     return render_template("home.html")
+
+
+@app.route('/posts')
+def posts():
+    return render_template("posts.html")
+
+
+@app.route('/albums')
+def albums():
+    return render_template("albums.html")
+
 
 if __name__ == '__main__':
     app.run(debug=True)
